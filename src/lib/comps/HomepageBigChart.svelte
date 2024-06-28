465d4aa3593f71c208cea2975c4fd984b44f07c1
JakubBlaha-IDS <jakubblaha@ids.company>
2024-06-28 13:12:46 +0200

    feat: fix big chart on homepage

---
+++ src/lib/comps/HomepageBigChart.svelte
@@ -0,0 +1,93 @@
+<script>
+	import { onMount } from 'svelte';
+	import { getGreedData } from '$lib/utils/mockChartData';
+	import Chart from 'chart.js/auto';
+
+	let chart;
+
+	let chartData = {
+		labels: [],
+		datasets: []
+	};
+
+	const color = '#47A663';
+	const data = getGreedData();
+
+	onMount(() => {
+		const ctx = canvas.getContext('2d');
+		const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+
+		gradient.addColorStop(0, 'rgba(71, 166, 99, 0.25)');
+		gradient.addColorStop(1, 'rgba(71, 166, 99, 0)');
+
+		chartData = {
+			labels: data.labels,
+			datasets: [
+				{
+					label: 'Value',
+					data: data.data,
+					borderColor: color,
+					borderWidth: 2,
+					pointRadius: 0,
+					fill: true,
+					backgroundColor: gradient
+				}
+			]
+		};
+
+		const chart = new Chart(ctx, {
+			type: 'line',
+			data: chartData,
+			options: options
+		});
+	});
+
+	const options = {
+		responsive: true,
+		maintainAspectRatio: false,
+		scales: {
+			y: {
+				beginAtZero: true,
+				grid: {
+					display: false // Remove background grid
+				},
+				ticks: {
+					display: true
+				},
+				border: {
+					display: false
+				},
+				min: 0,
+				max: 100,
+				step: 20,
+				color: '#FFFFFF'
+			},
+			x: {
+				beginAtZero: true,
+				grid: {
+					display: false // Remove background grid
+				},
+				ticks: {
+					display: false
+				},
+				border: {
+					display: false
+				}
+			}
+		},
+		plugins: {
+			legend: {
+				display: false
+			},
+			tooltip: {
+				enabled: false
+			}
+		}
+	};
+
+	$: console.log('chartData', chartData);
+
+	let canvas;
+</script>
+
+<canvas bind:this={canvas} width="400" height="150" />
