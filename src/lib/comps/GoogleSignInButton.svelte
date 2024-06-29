cfa7ca774e584bf19e37b00e1bb8bf10d4915d32
JakubBlaha-IDS <jakubblaha@ids.company>
2024-06-29 17:36:30 +0200

    feat: google login

--- src/lib/comps/GoogleSignInButton.svelte
+++ src/lib/comps/GoogleSignInButton.svelte
@@ -1,5 +1,19 @@
+<script lang="ts">
+	import type { SupabaseClient } from '@supabase/supabase-js';
+	import { getContext } from 'svelte';
+
+	const supabase = getContext<SupabaseClient>('supabase');
+
+	function signInWithGoodle() {
+		supabase.auth.signInWithOAuth({
+			provider: 'google'
+		});
+	}
+</script>
+
 <button
 	class="flex gap-x-2 font-semibold items-center justify-center text-[#FFFFFFCC] text-opacity-80 text-sm duration-100 h-[45px] px-4 py-2 rounded-[10px] bg-[#17151499] hover:bg-[#FFFFFF26] active:text-[#FFFFFF99] border-[#FFFFFF1A] hover:border-[#FFFFFF4D] border-opacity-10 hover:border-opacity-40 border-[0.5px] w-full"
+	on:click={signInWithGoodle}
 >
 	<img src="/icons/google.svg" alt="Google logo" width={18} height={18} />
 	Sign in with Google
