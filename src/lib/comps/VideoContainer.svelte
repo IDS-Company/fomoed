efe91ea87e453b6569914b2a77a61a9cd0db2fbb
JakubBlaha-IDS <jakubblaha@ids.company>
2024-07-07 21:27:15 +0200

    feat: remove video play button

--- src/lib/comps/VideoContainer.svelte
+++ src/lib/comps/VideoContainer.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div
-	class="bg-gradient-to-b from-[#FFFFFF24] to-[#FFFFFF0A] p-px w-full h-full rounded-[25px] relative overflow-hidden"
+	class="bg-gradient-to-b from-[#FFFFFF24] to-[#FFFFFF0A] p-px w-full h-full rounded-[25px] relative overflow-hidden brightness-75"
 >
 	<img src={thumbnail} width={1018} height={747} alt="Video thumbnail." class="rounded-[24.5px]" />
 
-	<div class="absolute inset-0 bg-black/70 flex items-center justify-center">
+	<!-- <div class="absolute inset-0 bg-black/70 flex items-center justify-center">
 		<img
 			src="/icons/play.svg"
 			width={100}
@@ -15,5 +15,5 @@
 			alt="Play."
 			class="hover:scale-105 duration-100 cursor-pointer"
 		/>
-	</div>
+	</div> -->
 </div>
