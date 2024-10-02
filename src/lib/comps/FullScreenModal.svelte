4692cdcac514c86f36fc2404279cf2d7300331e7
JakubBlaha-IDS <jakubblaha@ids.company>
2024-10-02 22:31:52 +0200

    fix: z index

--- src/lib/comps/FullScreenModal.svelte
+++ src/lib/comps/FullScreenModal.svelte
@@ -10,7 +10,7 @@
 		class="-desktop:px-2 -desktop:pb-2 -desktop:pt-14
 		desktop:px-[30px] desktop:py-[22px] bg-[#0f0d0d] desktop:rounded-[26px] desktop:border border-white border-opacity-10 grid place-items-center w-full h-full relative overscroll-none"
 	>
-		<div class="aspect-square absolute top-4 right-4">
+		<div class="aspect-square absolute top-4 right-4 z-10">
 			<button
 				class="w-8 h-8 p-1 active:brightness-50 duration-100 active:scale-90"
 				on:click={() => dispatch('close')}
