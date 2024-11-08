188689ab3b2d7b4cfca046868dd302eb235a737b
JakubBlaha-IDS <jakubblaha@ids.company>
2024-11-08 17:40:24 +0100

    fix

--- src/routes/(app)/api/ex-liq-map/+server.ts
+++ src/routes/(app)/api/ex-liq-map/+server.ts
@@ -10,6 +10,8 @@ import { createServerClient } from '@supabase/ssr';
 import { type RequestEvent, error, json } from '@sveltejs/kit';
 import { now } from 'lodash-es';
 
+export const maxDuration = 60;
+
 const maxCacheAgeSeconds = 120;
 
 /** @type {import('./$types').RequestHandler} */
